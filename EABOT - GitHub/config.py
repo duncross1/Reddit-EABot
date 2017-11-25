c17c3a5343fda6086960c1e5ae627a95707140a8
duncross1 <33895458+duncross1@users.noreply.github.com>
2017-11-25 17:27:00 +0000

    Update config.py

--- EABOT - GitHub/config.py
+++ EABOT - GitHub/config.py
@@ -1,5 +1,5 @@
 username = "[USERNAME]"#replace [USERNAME] with bot account username
-password = "[PASSWORD]"#replace [PASSWORD] with bot password username
+password = "[PASSWORD]"#replace [PASSWORD] with bot account password
 client_id = "[CLIENT ID]"#replace [CLIENT ID] with bot account client id
 client_secret = "[CLIENT KEY]"#replace [CLIENT KEY] with bot account client key
 
